414072fe130d4329c26556515e7d2e0f565d6333
Vitaly Sukhinin <vitaliksuhinin99@gmail.com>
2025-03-19 13:01:21 -0400

    added task list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {HeaderComponent} from "./components/header/header.component";
+import {TaskListComponent} from "./components/task-list/task-list.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, TaskListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'lab8';
+  taskList = ["Do laundry", "Buy groceries", "Pass exams"];
 }
